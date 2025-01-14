c07bbf8d974784ff0c9b5a714b60a488801642ba
Carrotist <romark99@gmail.com>
2025-01-14 17:10:06 +0300

    Added text colors changing, errors in unit testing

--- tests/main.test.js
+++ tests/main.test.js
@@ -1,5 +1,5 @@
 import { randNumber } from '@ngneat/falso';
-import { sum } from '../src/main';
+import { sum, getColor, colors } from '../src/main';
 
 test('sum(a)(b) must be equal to a + b', () => {
   const num1 = randNumber();
@@ -9,4 +9,14 @@ test('sum(a)(b) must be equal to a + b', () => {
 
 test('sum(5)(4) must be equal to 9', () => {
   expect(sum(5)(4)).toBe(9);
+});
+
+test('getColor returns correct color based on index', () => {
+  expect(getColor(0)).toBe('magenta');
+  expect(getColor(1)).toBe('cyan');
+  expect(getColor(2)).toBe('firebrick');
+  expect(getColor(3)).toBe('springgreen');
+  expect(getColor(4)).toBe('skyblue');
+  const randColor = randNumber() % colors.length;
+  expect(getColor(randColor)).toBe(colors[randColor]);
 });
