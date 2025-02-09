376ae2a4379be3d11149e84f82c05f03a70c0025
Carrotist <romark99@gmail.com>
2025-02-09 19:31:32 +0300

    Added hotels array, searchHotel() (regular expressions)

--- src/functions.js
+++ src/functions.js
@@ -1,6 +1,20 @@
+import { data } from './hotelsArray.js';
+
 export const convertDate = (date) => {
   const re = /(\d{4})-(\d{2})-(\d{2})/;
   return date.replace(re, '$3.$2.$1');
+};
+
+export const searchHotel = (str) => {
+  let hotels = [];
+  const re = new RegExp(str, 'i');
+  for (let i = 0; i < data.length; i++) {
+    const place = [data[i].country, data[i].city, data[i].hotel].join(', ');
+    if (re.test(place)) {
+      hotels.push(place);
+    }
+  }
+  return hotels;
 };
 
 export const sum = (num1) => {
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
-import { convertDate, getColor, sum } from './functions.js';
+import { convertDate, getColor, searchHotel, sum } from './functions.js';
 
 const date = '2020-11-26';
 console.log(convertDate(date)); // '26.11.2020'
+
+console.log(searchHotel('hotel'));
 
 const text1 = document.getElementById('text1');
 const text2 = document.getElementById('text2');
