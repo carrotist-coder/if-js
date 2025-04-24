1bbf1ad7d37fcaa06d40870dc7eb8e61657f900e
Carrotist <romark99@gmail.com>
2025-04-24 22:16:53 +0300

    Feat: fetching for Home guests love

--- src/utils/imageRendering.js
+++ src/utils/imageRendering.js
@@ -1,20 +1,31 @@
-import { data } from '../consts/hotelsArray.js';
-
 document.addEventListener('DOMContentLoaded', () => {
   const container = document.querySelector('.homes-guests .cards');
 
-  data.forEach((item) => {
-    const card = document.createElement('div');
-    card.classList.add('homes-guests-card');
-    card.innerHTML = `
-      <figure>
-        <img src="${item.imageUrl}" alt="${item.name} picture" />
-        <figcaption>
-          <p>${item.name}</p>
-          <p>${item.city}, ${item.country}</p>
-        </figcaption>
-      </figure>
-    `;
-    container.appendChild(card);
-  });
+  fetch('https://if-student-api.onrender.com/api/hotels/popular')
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Error status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      data.forEach((item) => {
+        const card = document.createElement('div');
+        card.classList.add('homes-guests-card');
+        card.innerHTML = `
+          <figure>
+            <img src="${item.imageUrl}" alt="${item.name} picture" />
+            <figcaption>
+              <p>${item.name}</p>
+              <p>${item.city}, ${item.country}</p>
+            </figcaption>
+          </figure>
+        `;
+        container.appendChild(card);
+      });
+    })
+    .catch((error) => {
+      console.error('Fetch error: ', error);
+      container.innerHTML = '<p>Не удалось загрузить данные.</p>';
+    });
 });
