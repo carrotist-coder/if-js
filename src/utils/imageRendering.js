05012e7efdef44fc525e32d24ca7258219cb7ecd
Carrotist <romark99@gmail.com>
2025-05-17 00:08:05 +0300

    Minor changes

--- src/utils/imageRendering.js
+++ src/utils/imageRendering.js
@@ -1,31 +1,39 @@
+const renderPopularHotelCard = (hotel, container) => {
+  const card = document.createElement('div');
+  card.classList.add('homes-guests-card');
+  card.innerHTML = `
+    <figure>
+      <img src="${hotel.imageUrl}" alt="${hotel.name} picture" />
+      <figcaption>
+        <p>${hotel.name}</p>
+        <p>${hotel.city}, ${hotel.country}</p>
+      </figcaption>
+    </figure>
+  `;
+  container.appendChild(card);
+};
+
+const renderError = (container) => {
+  container.innerHTML = '<p>Не удалось загрузить данные.</p>';
+};
+
+const fetchPopularHotels = async (container) => {
+  try {
+    const response = await fetch(
+      'https://if-student-api.onrender.com/api/hotels/popular',
+    );
+    if (!response.ok) {
+      throw new Error(`Error status: ${response.status}`);
+    }
+    const data = await response.json();
+    data.forEach((hotel) => renderPopularHotelCard(hotel, container));
+  } catch (error) {
+    console.error('Fetch error: ', error);
+    renderError(container);
+  }
+};
+
 document.addEventListener('DOMContentLoaded', () => {
   const container = document.querySelector('.homes-guests .cards');
-
-  fetch('https://if-student-api.onrender.com/api/hotels/popular')
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(`Error status: ${response.status}`);
-      }
-      return response.json();
-    })
-    .then((data) => {
-      data.forEach((item) => {
-        const card = document.createElement('div');
-        card.classList.add('homes-guests-card');
-        card.innerHTML = `
-          <figure>
-            <img src="${item.imageUrl}" alt="${item.name} picture" />
-            <figcaption>
-              <p>${item.name}</p>
-              <p>${item.city}, ${item.country}</p>
-            </figcaption>
-          </figure>
-        `;
-        container.appendChild(card);
-      });
-    })
-    .catch((error) => {
-      console.error('Fetch error: ', error);
-      container.innerHTML = '<p>Не удалось загрузить данные.</p>';
-    });
+  fetchPopularHotels(container);
 });
